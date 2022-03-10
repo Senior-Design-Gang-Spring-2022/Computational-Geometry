5091eb57cc855d9eebadb13d5ccb63d56edb8188
AayushGuragain <59627010+AayushGuragain@users.noreply.github.com>
2022-03-10 14:36:19 -0500

    Create points.py

---
+++ points.py
@@ -0,0 +1,13 @@
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def getx(self):
+        return self.x
+
+    def gety(self):
+        return self.y
+
+    def __str__(self):
+        return (self.x, self.y)
