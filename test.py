1cd565a59ff4aab281d631de43e27fa3a662248e
AayushGuragain <59627010+AayushGuragain@users.noreply.github.com>
2022-03-13 12:21:45 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ from points import Point
 c = Point(3, 5)
 
 def test_getx():
-    assert c.getx == 3
+    assert c.getx() == 3
 
 def test_gety():
-    assert c.gety == 5
+    assert c.gety() == 5
 
 def test_get_coord():
     assert c.get_coord() == (3, 5)
